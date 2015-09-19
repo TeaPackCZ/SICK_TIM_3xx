483018deefe17843cd94b6df72fc27d761201953
TeaPackCZ <honza.crha@gmail.com>
2015-09-20 00:47:26 +0200

    Improve comments of code
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- show_laser_numpy.py
+++ show_laser_numpy.py
@@ -2,15 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+
+## Preparation of variables
+
 base_src = "measurements/Measurement_"
 max_dif = 150
-number_of_files = 5
+number_of_files = 40
 
 ang_values = np.zeros((number_of_files,271),np.int16)
 x_values = np.zeros((number_of_files,271),np.float)
 y_values = np.zeros((number_of_files,271),np.float)
 val_diff = np.zeros((number_of_files-1,271),np.int16)
 axis_x = np.arange(-135,136,1,np.float)
+
+
+## Loading data:
 
 for i in range(number_of_files):
     name = base_src + str(i)
@@ -20,29 +26,8 @@ for i in range(number_of_files):
         ang_values[i][j] = int(str_data[j])
     files.close()
 
+
 ## Segmentation:
-##segm_starts = []
-##segm_len = []
-##segm_data = []
-##
-##for i in range(number_of_files):
-##    start_segm = 0
-##    end_segm = 270
-##    segm_starts.append([0])
-##    segm_data.append([])
-##    segm_len.append([])
-##    for j in range(270):
-##        diff = abs(ang_values[i][j]-ang_values[i][j+1])
-##        if diff > max_dif:
-##            segm_starts[i].append(j+1)
-##            segm_len[i].append(j+1 - start_segm)
-##            segm_data[i].append([ang_values[start_segm:start_segm+segm_len[-1][-1]]])
-##            start_segm = j+1
-##    segm_starts[i].append(j+1)
-##    segm_len[i].append(j+1 - start_segm)
-##    segm_data[i].append([ang_values[start_segm:start_segm+segm_len[-1][-1]]])
-##
-##npdata = np.asarray(segm_data[0])
 
 segments = []
 segm_len = []
@@ -69,15 +54,21 @@ for i in range(number_of_files):
 npdata = np.asarray(segments)
 
 
-## To cartesian:
+## Conversion to cartesian:
+
 for i in range(number_of_files):
     for j in range(271):
         x_values[i][j] = math.cos(axis_x[j]/180.0*math.pi)*ang_values[i][j]
         y_values[i][j] = math.sin(axis_x[j]/180.0*math.pi)*ang_values[i][j]
 
 
+## Diff computing:
+
 for i in range(number_of_files-1):
     val_diff[i] = ang_values[i+1]-ang_values[i]
+
+
+## Show data:
 
 plt.figure
 for i in range(number_of_files):

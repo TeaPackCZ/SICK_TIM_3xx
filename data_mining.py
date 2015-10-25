3a858accd2f1b1fdfcdf5472c917cc9d19751ba4
TeaPackCZ <honza.crha@gmail.com>
2015-10-25 19:43:14 +0100

    data_mining: Modify to test all new functions in coord_lib from already logged data
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- data_mining.py
+++ data_mining.py
@@ -1,12 +1,13 @@
+## File for testing on logged data
 
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-from coord_lib import ang2cartezian
+from coord_lib import *
 
-base_src = "measurements/Measurement_"
+base_src = "measurements1/measurement"
 max_dif = 150
-number_of_files = 5
+number_of_files = 1
 
 ang_values = np.zeros(271,np.int16)
 axis_x = np.arange(-135,136,1,np.float)
@@ -14,7 +15,7 @@ axis_x = np.arange(-135,136,1,np.float)
 first_run = True
 
 for i in range(number_of_files):
-    name = base_src + str(i)
+    name = base_src + str(18)
     files = open(name,"r")
     str_data = files.readline().replace(" ","").replace("[","").replace("]","").split(",")
     for j in range(len(str_data)):
@@ -34,11 +35,11 @@ for i in range(number_of_files):
         old_x = new_x
         old_y = new_y
         old_segments = new_segments
-        old_segm_len = new_segm_len
-        old_segm_start = new_segm_start
-        old_np_data = np.copy(new_np_data)
-        del new_angle, new_x, new_y, new_segments, new_segm_len, new_segm_start
-        del new_np_data
+        try:
+            del new_angle, new_x, new_y, new_segments, new_segm_len, new_segm_start
+            del new_np_data
+        except:
+            pass
     else:
         first_run = False
 
@@ -46,43 +47,31 @@ for i in range(number_of_files):
     [new_x,new_y]=ang2cartezian(axis_x,new_angle)
     
     ## Segmentation:
-    new_segments = []
-    new_segm_len = []
-    new_segm_start = []
+    new_segments = ang_segmentation(ang_values, max_diff = 200)
 
-    new_segm_start.append(0)
-    start_segm = 0
-    for j in range(270):
-        diff = abs(new_angle[j]-new_angle[j+1])
-        if diff > max_dif:
-            if(start_segm == j):
-                # throw out single points
-                start_segm = j+1
-                pass
-            else:
-                end_segm = j
-                new_segments.append(new_angle[start_segm:end_segm+1])
-                new_segm_start.append(j)
-                new_segm_len.append(len(new_angle[start_segm:end_segm+1]))
-                start_segm = j+1
-
-    new_segments.append(new_angle[start_segm:-1])
-    new_segm_start.append(start_segm)
-    new_segm_len.append(len(new_angle[start_segm:-1]))
-
-    ## convert to np array
-    new_np_data = np.asarray(new_segments)
-
+    ## convert segments to cartezian:
+    new_x = []
+    new_y = []
+    for j in range(len(new_segments)):
+        [nx,ny] = ang2cartezian(axis_x[new_segments[j][0]:new_segments[j][0]+new_segments[j][1]],new_segments[j][2])
+        new_x.append(nx)
+        new_y.append(ny)
+    
     ## TODO: script for finding human legs near [0,3500] +-[500,500]
     
     ## Show data:
-    plt.figure(0)
-    for j in range(len(new_segm_start)-1):
-        if(new_segm_len[j] > 1):
-            plt.plot(axis_x[new_segm_start[j]:new_segm_start[j]+new_segm_len[j]],new_segments[j])
+    plt.figure(i)
+    #Angular data:
+    for j in range(len(new_segments)):
+        if(new_segments[j][1] > 1):
+            plt.plot(axis_x[new_segments[j][0]:new_segments[j][0]+new_segments[j][1]],new_segments[j][2])
         else:
-            plt.plot(axis_x[segm_start[j]:segm_start[j]+segm_len[j]],segments[j],"*")
-    plt.show()
+            plt.plot(axis_x[new_segments[j][0]:new_segments[j][0]+new_segments[j][1]],new_segments[j][2],"*")
+    # Cartezian data:
+    plt.figure(i+number_of_files)
+    for j in range(len(new_segments)):
+        plt.plot(new_x[j],new_y[j])
+plt.show()
     
 
 

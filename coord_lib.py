36ff453d24ef376aa31fa73484dce9ad68e5ce2b
TeaPackCZ <honza.crha@gmail.com>
2015-10-25 19:34:36 +0100

    Coord_lib: Add check for consistency of input data
    Signed-off-by: TeaPackCZ <honza.crha@gmail.com>

--- coord_lib.py
+++ coord_lib.py
@@ -3,6 +3,12 @@ import numpy as np
 
 def ang2cartezian(axis,distance):
     k = len(distance)
+    if(len(axis) == len(distance)):
+        pass
+    else:
+        print "Error: inputs has different lengths"
+        print len(axis), len(distance)
+        return 0,0
     x = np.zeros(k,np.int16)
     y = np.copy(x)
     for j in range(k):
